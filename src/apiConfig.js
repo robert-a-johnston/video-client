4991accae7f56d3ed3cfda8bcb52e74b7dac1a8b
ar[arnau321 <arnau123@att.net>
2022-02-03 09:38:48 -0500

    Start auth

---
+++ src/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl
+const apiUrls = {
+  production: '',
+  development: 'http://localhost:5000'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+export default apiUrl
